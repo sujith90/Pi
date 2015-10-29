27e1a0ced43378ada2f4e421eff0de04f6b84008
Sujith Pottem <sujith90@gmail.com>
2015-10-28 17:00:50 -0700

    Updated to rotate with omron detect

--- Omron/thermal-display.py
+++ Omron/thermal-display.py
@@ -3,8 +3,17 @@
 import pygame, time, sys
 from omrond6t import *
 from pygame.locals import *
+import RPi.GPIO as GPIO
 
 omron = OmronD6T(arraySize=16)
+
+
+#GPIO Initializers
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+p = GPIO.PWM(18, 50)  # channel=12 frequency=50Hz
+p.start(0)
+p.ChangeDutyCycle(70.0/10.0)
 
 SCREEN_DIMS = [500, 500]
 xSize = 4 
@@ -39,15 +48,15 @@ for x in range(xSize):
 for y in range(ySize):
     Y.append(y*cellWidth) #(y * cellHeight) + (SCREEN_DIMS[1] - cellHeight))
 
-for x in range(xSize):
-  for y in range(ySize):
+for y in range(ySize):
+  for x in range(xSize):
     square.append((X[x], Y[y], cellWidth, cellHeight))
     center.append((X[x] + cellWidthCenter, Y[y] + cellHeightCenter))
 
 def temp_to_rgb(temp):
-  if temp < 80:
+  if temp < 76:
     return (0, 0, 192)
-  elif temp >= 80 and temp < 90:
+  elif temp >= 76 and temp < 90:
     return (255, 128, 0)
   elif temp > 90:
     return (255, 0, 0)
@@ -61,6 +70,63 @@ text_pos = text.get_rect()
 text_pos.center = ((SCREEN_DIMS[0])/4,SCREEN_DIMS[1] - cellHeight - 18)
 screen.blit(text, text_pos)
 
+
+# define the function blocks
+def center_block():
+    p.ChangeDutyCycle(7)
+    print "Center Block: Turn on LED & No movement"
+
+def top_center():
+    #GPIO.output(17, GPIO.LOW)
+    print "Top Center: Turn off LED & Move Camera UP"
+    
+def bottom_center():
+    #GPIO.output(17, GPIO.LOW)
+    print "Bottom Center: Turn off LED & Move Camera DOWN"
+
+def right_center():
+    p.ChangeDutyCycle(10.5)
+    print "Right Center: Turn off LED & Move Camera RIGHT"
+    
+def left_center():
+    p.ChangeDutyCycle(4.5)
+    print "Left Center: Turn off LED & Move Camera LEFT"
+
+def top_left():
+    p.ChangeDutyCycle(4.5)
+    print "Top Left: Turn off LED & Move Camera UP_LEFT"
+
+def top_right():
+    p.ChangeDutyCycle(10.5)
+    print "Top Right: Turn off LED & Move Camera UP_RIGHT"
+
+def bottom_left():
+    p.ChangeDutyCycle(4.5)
+    print "Bottom Left: Turn off LED & Move Camera BOTTOM_LEFT"
+    
+def bottom_right():
+    p.ChangeDutyCycle(10.5)
+    print "Bottom Right: Turn off LED & Move Camera BOTTOM_RIGHT"
+
+# map the inputs to the function blocks
+function = {
+            0 : top_left,
+            1 : top_center,
+            2 : top_center,
+            3 : top_right,
+            4 : left_center,
+            5 : center_block,
+            6 : center_block,
+            7 : right_center,
+            8 : left_center,
+            9 : center_block,
+            10 : center_block,
+            11 : right_center,
+            12 : bottom_left,
+            13 : bottom_center,
+            14 : bottom_center,
+            15 : bottom_right
+}
 
 while True:
   for event in pygame.event.get():
@@ -76,11 +142,18 @@ while True:
 
   temp_hit = 0
   for i in range(arraySize):
-    if temperature[i] >= 80:
+    if temperature[i] >= 76:
       temp_hit += 1
     
     print(temperature[i],  i) 
     screen.fill(temp_to_rgb(temperature[i]), square[i])
+    if max(temperature) > 76:
+	    cell = temperature.index(max(temperature))
+	    function[cell]()
+    else:
+	p.ChangeDutyCycle(7.0)
+ 
+    
     
     #text = font.render(str(i+1), 12, (255,255,255))
     #text_pos = text.get_rect()
@@ -107,6 +180,8 @@ while True:
     text_pos = text.get_rect()
     text_pos.center = (SCREEN_DIMS[0]/2,90)
     screen.blit(text, text_pos)
+    
+
   else:
     #screen.fill((0,0,0), (0,180,SCREEN_DIMS[0],180))
     #screen.fill((0,192,0), (0,0,SCREEN_DIMS[0],180))
@@ -114,6 +189,7 @@ while True:
     text_pos = text.get_rect()
     text_pos.center = (SCREEN_DIMS[0]/2,90)
     screen.blit(text, text_pos)
+    
 
   pygame.display.update()
   time.sleep(0.01)
