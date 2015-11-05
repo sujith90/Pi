2ceb5eece72b6ad2852e924e3966ae8412b3fb99
Sujith Pottem <sujith90@gmail.com>
2015-11-04 21:48:24 -0800

    Sujith updates for monkeyhead

--- Omron/thermal-display.py
+++ Omron/thermal-display.py
@@ -5,7 +5,12 @@ from omrond6t import *
 from pygame.locals import *
 import RPi.GPIO as GPIO
 
+
 omron = OmronD6T(arraySize=16)
+
+time_delay = .05
+temp_difference = 13
+
 
 
 #GPIO Initializers
@@ -14,6 +19,7 @@ GPIO.setup(18, GPIO.OUT)
 p = GPIO.PWM(18, 50)  # channel=12 frequency=50Hz
 p.start(0)
 p.ChangeDutyCycle(70.0/10.0)
+time.sleep(time_delay)
 
 SCREEN_DIMS = [500, 500]
 xSize = 4 
@@ -72,60 +78,66 @@ screen.blit(text, text_pos)
 
 
 # define the function blocks
-def center_block():
-    p.ChangeDutyCycle(7)
-    print "Center Block: Turn on LED & No movement"
 
-def top_center():
-    #GPIO.output(17, GPIO.LOW)
-    print "Top Center: Turn off LED & Move Camera UP"
+
+def Cell_One():
+    row = [temperature[maxCell]]
+    row.insert(1,temperature[maxCell+1])
+    row.insert(2,temperature[maxCell+2])
+    row.insert(3,temperature[maxCell+3])
+#multiply  based on max-min
+    setDutyCycle(row)
+
+
+def Cell_Two():
+    row = [temperature[maxCell-1]]
+    row.insert(1,temperature[maxCell])
+    row.insert(2,temperature[maxCell+1])
+    row.insert(3,temperature[maxCell+2])
+    setDutyCycle(row)
+
+def Cell_Three():
+    row = [temperature[maxCell-2]]
+    row.insert(1,temperature[maxCell-1])
+    row.insert(2,temperature[maxCell])
+    row.insert(3,temperature[maxCell+1])
+    setDutyCycle(row)
     
-def bottom_center():
-    #GPIO.output(17, GPIO.LOW)
-    print "Bottom Center: Turn off LED & Move Camera DOWN"
+def Cell_Four():
+    row = [temperature[maxCell-3]]
+    row.insert(1,temperature[maxCell-2])
+    row.insert(2,temperature[maxCell-1])
+    row.insert(3,temperature[maxCell])
+    setDutyCycle(row)
 
-def right_center():
-    p.ChangeDutyCycle(10.5)
-    print "Right Center: Turn off LED & Move Camera RIGHT"
+def setDutyCycle(values):
+    print "Temp Value Read: ", ["%.2f" % member for member in values]
+
+    if((max(values)-min(values)) > temp_difference):
+	multiplier = [values[0]-min(values),values[1]-min(values),values[2]-min(values),values[3]-min(values)]
+	mValues = [multiplier[0]*values[0],multiplier[1]*values[1],multiplier[2]*values[2],multiplier[3]*values[3]]
+	mulAvg  = sum(mValues)/len(mValues)
+	weightedValue = [mValues[0]/mulAvg, mValues[1]/mulAvg, mValues[2]/mulAvg, mValues[3]/mulAvg]
+
+	ServoValues = [weightedValue[0]*5,weightedValue[1]*7,weightedValue[2]*8,weightedValue[3]*10]
+
+	DC = sum(ServoValues)/len(ServoValues)
+    else:
+    	print "Row: ", values, "\tMax-Min: ", (max(values)-min(values))
+	DC = 7.5
+
+    p.ChangeDutyCycle(DC)
+    time.sleep(time_delay)
+
+    print "DC: ", DC, "\n"
     
-def left_center():
-    p.ChangeDutyCycle(4.5)
-    print "Left Center: Turn off LED & Move Camera LEFT"
-
-def top_left():
-    p.ChangeDutyCycle(4.5)
-    print "Top Left: Turn off LED & Move Camera UP_LEFT"
-
-def top_right():
-    p.ChangeDutyCycle(10.5)
-    print "Top Right: Turn off LED & Move Camera UP_RIGHT"
-
-def bottom_left():
-    p.ChangeDutyCycle(4.5)
-    print "Bottom Left: Turn off LED & Move Camera BOTTOM_LEFT"
-    
-def bottom_right():
-    p.ChangeDutyCycle(10.5)
-    print "Bottom Right: Turn off LED & Move Camera BOTTOM_RIGHT"
 
 # map the inputs to the function blocks
 function = {
-            0 : top_left,
-            1 : top_center,
-            2 : top_center,
-            3 : top_right,
-            4 : left_center,
-            5 : center_block,
-            6 : center_block,
-            7 : right_center,
-            8 : left_center,
-            9 : center_block,
-            10 : center_block,
-            11 : right_center,
-            12 : bottom_left,
-            13 : bottom_center,
-            14 : bottom_center,
-            15 : bottom_right
+            1 : Cell_One,
+            2 : Cell_Two,
+            3 : Cell_Three,
+            0 : Cell_Four
 }
 
 while True:
@@ -139,19 +151,26 @@ while True:
         sys.exit(0)
 
   bytes_read, temperature = omron.read()
-
+  #Something is wrong with the room temp. It is much greater than the other cells
+  #print omron.roomTemp
   temp_hit = 0
+  maxCell = -1
   for i in range(arraySize):
     if temperature[i] >= 76:
       temp_hit += 1
     
-    print(temperature[i],  i) 
+    #print(temperature[i],  i) 
     screen.fill(temp_to_rgb(temperature[i]), square[i])
-    if max(temperature) > 76:
-	    cell = temperature.index(max(temperature))
-	    function[cell]()
+    #Can this calculation be done before the loop? BP
+    if max(temperature) > 70:
+	    maxCell = temperature.index(max(temperature))
+	    #print ["%.2f" % member for member in temperature]
+	    #print maxCell, ": ", temperature[maxCell], "\n"
+	    print "\n"
+	    function[(maxCell+1)%4]()
     else:
 	p.ChangeDutyCycle(7.0)
+	time.sleep(time_delay)
  
     
     
@@ -159,7 +178,11 @@ while True:
     #text_pos = text.get_rect()
     #text_pos.center = (center[i][0], SCREEN_DIMS[1] - cellHeight + 18)
     #screen.blit(text, text_pos)
-    text = font.render(str(int(temperature[i]))+" "+str(i+1) + chr(0xb0) + "F", 1, (255,255,255))
+    
+    if maxCell !=-1 & i != maxCell:
+    	text = font.render(str(int(temperature[i]))+" "+str(i+1) + chr(0xb0) + "F", 1, (255,255,255))
+    else:
+	text = font.render(str(int(temperature[i]))+" "+str(i+1) + chr(0xb0) + "F", 1, (0,0,0)) 
     text_pos = text.get_rect()
     text_pos.center = center[i]
     screen.blit(text, text_pos)
@@ -175,7 +198,7 @@ while True:
 
   if person_detect:    
     #screen.fill((0,0,0), (0,180,SCREEN_DIMS[0],180))
-    screen.fill((255,0,0), (0,0,SCREEN_DIMS[0],180))
+    #screen.fill((255,0,0), (0,0,SCREEN_DIMS[0],180))
     text = font2.render('RESERVED', 1, (255,255,255))
     text_pos = text.get_rect()
     text_pos.center = (SCREEN_DIMS[0]/2,90)
@@ -183,8 +206,6 @@ while True:
     
 
   else:
-    #screen.fill((0,0,0), (0,180,SCREEN_DIMS[0],180))
-    #screen.fill((0,192,0), (0,0,SCREEN_DIMS[0],180))
     text = font2.render('AVAILABLE', 1, (255,255,255))
     text_pos = text.get_rect()
     text_pos.center = (SCREEN_DIMS[0]/2,90)
