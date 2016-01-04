022d2606ea4b2684605f1a967954f140bd1f1964
Sujith Pottem <sujith90@gmail.com>
2016-01-03 19:03:05 -0800

    updated rgb code

--- sandbox_code/rgb.py
+++ sandbox_code/rgb.py
@@ -1,13 +1,19 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(33, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
 while True:
-	GPIO.output(13, 1) 
-	GPIO.output(33, 1)
-	GPIO.output(10, 1)
-
+	GPIO.output(23, 1) 
+	GPIO.output(24, 1)
+	GPIO.output(25, 1)
+	time.sleep(2)
+	
+	GPIO.output(23, 0) 
+	GPIO.output(24, 0)
+	GPIO.output(25, 0)
+	time.sleep(2)	
